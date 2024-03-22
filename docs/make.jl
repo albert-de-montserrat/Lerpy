33e2acdb60dd902ea1798005c81615b79e010f10
Albert de Montserrat <albert.de.montserrat@gmail.com>
2024-03-22 22:23:57 +0100

    up make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,7 +1,12 @@
 push!(LOAD_PATH,"../src/")
 
 using Documenter, Lerpy
-makedocs(sitename="My Documentation")
+makedocs(
+    sitename="Lerpy.jl",
+    pages=[
+        "Home" => "index.md",
+    ]
+)
 
 deploydocs(
     repo = "https://github.com/albert-de-montserrat/Lerpy.jl",
